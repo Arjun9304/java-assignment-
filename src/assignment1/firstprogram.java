bd1da27dc94a0c5740b9d28f538ace5cc32707ed
chhotukumar321 <114070771+chhotukumar321@users.noreply.github.com>
2022-09-27 09:57:41 +0530

    assignment file added

---
+++ src/assignment1/firstprogram.java
@@ -0,0 +1,18 @@
+package assignment1;
+
+
+public class firstprogram {
+
+	static String FirstName = "chhotu";
+	static String LastNAME = "kumar";
+	static int age = 22;
+	static {
+		System.out.println(FirstName);
+		System.out.println(LastNAME);
+		System.out.println(age);
+	}
+
+
+public static void main(String[] args) {
+}
+}
