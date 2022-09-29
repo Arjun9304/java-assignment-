f37a5c49bc67726dad1c0bdb0a1322ac3908233e
chhotukumar321 <114070771+chhotukumar321@users.noreply.github.com>
2022-09-29 15:36:45 +0530

    assignment3 added

---
+++ src/assignment3/q1.java
@@ -0,0 +1,24 @@
+package assignment3;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class q1 {
+	public static void main(String[] args) {
+
+		List<Float> list = new ArrayList<>();
+
+		list.add((float) 1.1);
+		list.add((float) 1.2);
+		list.add((float) 1.3);
+		list.add((float) 1.4);
+		list.add((float) 1.5);
+		float sum = 0;
+		Iterator<Float> it = list.iterator();
+		while (it.hasNext()) {
+			sum = sum + it.next();
+		}
+		System.out.println(sum);
+	}
+}
