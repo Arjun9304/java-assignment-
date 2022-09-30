5f15062881de7c0cfd50c615e80d7790347993d3
chhotukumar321 <114070771+chhotukumar321@users.noreply.github.com>
2022-09-30 14:44:32 +0530

    assignment4added

---
+++ src/assignment4/Q5.java
@@ -0,0 +1,21 @@
+package assignment4;
+
+interface Clickable {
+    default void click() {
+        System.out.println("click");
+    }
+}
+
+interface Accessible {
+    default void access() {
+        System.out.println("access");
+    }
+}
+public class Q5 implements Clickable, Accessible {
+    public static void main(String[] args) {
+        Q5 button = new Q5();
+        button.click();
+        button.access();
+    }
+
+}
